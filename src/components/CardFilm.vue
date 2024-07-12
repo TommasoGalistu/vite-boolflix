8efa05d289e620143fd9110063a0b8b13c6af30a
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-07-12 12:16:36 +0200

    fix: transition in hover

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -35,58 +35,62 @@ export default {
 </script>
 
 <template>
-  <div v-if="info.tipo === 'film'" class="card">
-    <div class="contLocandina">
-      <div class="contImg">
-        <img
-          :src="`http://image.tmdb.org/t/p/original${info.valore.poster_path}`"
-          :alt="`Poster di '${info.valore.title}'`"
-        />
+  <div v-if="info.tipo === 'film'" class="card flip-card">
+    <div class="flip-card-inner">
+      <div class="contLocandina flip-card-front">
+        <div class="contImg">
+          <img
+            :src="`http://image.tmdb.org/t/p/original${info.valore.poster_path}`"
+            :alt="`Poster di '${info.valore.title}'`"
+          />
+        </div>
       </div>
-    </div>
-    <div class="textContent">
-      <h2>{{ info.valore.title }}</h2>
-      <h4>{{ info.valore.original_title }}</h4>
-      <img
-        :src="info.lingua.src"
-        :srcset="info.lingua.srcset"
-        :alt="info.lingua.alt"
-      />
-      <p>{{ `Voto medio ${Math.floor(info.valore.vote_average)}/10` }}</p>
-      <div>
-        <font-awesome-icon
-          v-for="(icone, index) in numberOfStar(info.valore.vote_average)"
-          :key="index"
-          :icon="icone"
+      <div class="textContent flip-card-back">
+        <h2>{{ info.valore.title }}</h2>
+        <h4>{{ info.valore.original_title }}</h4>
+        <img
+          :src="info.lingua.src"
+          :srcset="info.lingua.srcset"
+          :alt="info.lingua.alt"
         />
+        <p>{{ `Voto medio ${Math.floor(info.valore.vote_average)}/10` }}</p>
+        <div>
+          <font-awesome-icon
+            v-for="(icone, index) in numberOfStar(info.valore.vote_average)"
+            :key="index"
+            :icon="icone"
+          />
+        </div>
       </div>
     </div>
   </div>
 
-  <div v-else-if="info.tipo === 'serie'" class="card">
-    <div class="contLocandina">
-      <div class="contImg">
-        <img
-          :src="`http://image.tmdb.org/t/p/original${info.valore.poster_path}`"
-          :alt="`Poster di '${info.valore.name}'`"
-        />
+  <div v-else-if="info.tipo === 'serie'" class="card flip-card">
+    <div class="flip-card-inner">
+      <div class="contLocandina flip-card-front">
+        <div class="contImg">
+          <img
+            :src="`http://image.tmdb.org/t/p/original${info.valore.poster_path}`"
+            :alt="`Poster di '${info.valore.name}'`"
+          />
+        </div>
       </div>
-    </div>
-    <div class="textContent">
-      <h2>{{ info.valore.name }}</h2>
-      <h4>{{ info.valore.original_name }}</h4>
-      <img
-        :src="info.lingua.src"
-        :srcset="info.lingua.srcset"
-        :alt="info.lingua.alt"
-      />
-      <p>{{ info.valore.vote_average }}</p>
-      <div>
-        <font-awesome-icon :icon="['far', 'star']" />
-        <font-awesome-icon :icon="['far', 'star']" />
-        <font-awesome-icon :icon="['far', 'star']" />
-        <font-awesome-icon :icon="['far', 'star']" />
-        <font-awesome-icon :icon="['fas', 'star']" />
+      <div class="textContent flip-card-back">
+        <h2>{{ info.valore.name }}</h2>
+        <h4>{{ info.valore.original_name }}</h4>
+        <img
+          :src="info.lingua.src"
+          :srcset="info.lingua.srcset"
+          :alt="info.lingua.alt"
+        />
+        <p>{{ `Voto medio ${Math.floor(info.valore.vote_average)}/10` }}</p>
+        <div>
+          <font-awesome-icon
+            v-for="(icone, index) in numberOfStar(info.valore.vote_average)"
+            :key="index"
+            :icon="icone"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -97,46 +101,74 @@ h3 {
   font-size: 3rem;
 }
 
-.card {
-  background-color: grey;
+.flip-card {
+  background-color: transparent;
   height: 30rem;
   width: calc((100% / 4) - 0.2rem);
-  // padding: 1rem;
-  position: relative;
+  perspective: 1000px;
 
-  &:hover .textContent {
-    display: block;
-  }
-  &:hover .contLocandina {
-    display: none;
-  }
-  .contLocandina {
-    height: 100%;
+  .flip-card-inner {
+    position: relative;
     width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 1s;
+    transform-style: preserve-3d;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  }
+
+  &:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+
+  .contLocandina,
+  .textContent {
     position: absolute;
-    top: 0;
-    left: 0;
-    .contImg {
-      width: 100%;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+  }
+
+  .contLocandina {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transform: rotateY(0deg);
+    background-color: #bbb;
+    color: black;
+  }
+
+  .textContent {
+    transform: rotateY(180deg);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: #2980b9;
+    color: white;
+    padding: 1rem;
+  }
+
+  .contImg {
+    width: 100%;
+    height: 100%;
+    img {
       height: 100%;
-      img {
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-      }
+      width: 100%;
+      object-fit: cover;
     }
   }
-  .textContent {
-    text-align: center;
-    display: none;
-    h2 {
-      padding: 1rem;
-    }
-    h4 {
-      padding: 0.5rem;
-    }
+
+  h2 {
+    padding: 1rem;
+  }
+
+  h4 {
+    padding: 0.5rem;
   }
 }
+
 .containerCard {
   background-color: black;
   width: 100%;
